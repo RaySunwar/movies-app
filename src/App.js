7fca7f544a833eca2f496e6bd1203bf1830ada58
RaySunwar <ray@whitehatagency.com.au>
2020-10-02 13:57:55 +0545

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Movie from './components/Movie/Movie';
+
+
+const MOVIES_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1`;
+const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(MOVIES_API)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+          setMovies(data.results)
+      })
+  }, [movies]);
+
+
   return (
-    <div className="App">
-      Hello World
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((movie) => (
+        < Movie key={movie.id} {...movie}  />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
