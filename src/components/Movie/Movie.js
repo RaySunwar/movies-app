c9778a9affb246be8d5820dc607026ccb7b90a3d
RaySunwar <ray@whitehatagency.com.au>
2020-10-06 11:02:30 +0545

    refactor: setup condition to check the vote rate

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,14 +2,23 @@ import React from "react";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+    if(vote >= 8){
+        return "green";
+    } else if (vote >= 6){
+        return "orange";
+    } else {
+        return "red"
+    }
+};
 
 const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
         <div className="movie">
-            < img src ={IMGPATH + poster_path} alt={title} />
+            < img src ={poster_path ? IMGPATH + poster_path : 'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'} alt={title} />
             <div className="movie-info">
                 <h3>{title}</h3>
-                <span>{vote_average}</span>
+                <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
             </div>
             <div className="movie-over">
                 <h1>overview:</h1>
